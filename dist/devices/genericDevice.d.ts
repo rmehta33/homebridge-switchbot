389204131770e0ad03d9b089d7a5be7740a2a47a
Rishuv Mehta <bmehta892@gmail.com>
2026-06-20 13:55:52 -0700

    Fix Curtain HomeKit BLE state handling

--- dist/devices/genericDevice.d.ts
+++ dist/devices/genericDevice.d.ts
@@ -66,15 +66,26 @@ export declare class BotDevice extends GenericDevice {
     createHAPAccessory(api: any): any;
 }
 export declare class CurtainDevice extends GenericDevice {
+    private lastKnownPosition;
+    private lastTargetPosition;
+    private positionState;
+    private preferLocalPositionUntil;
+    private clampHomeKitPosition;
+    private toHomeKitPosition;
+    private toSwitchBotPosition;
+    private getPositionForHomeKit;
     createHAPAccessory(api: any): {
         services: {
             type: string;
             characteristics: {
                 CurrentPosition: {
-                    get: () => Promise<any>;
+                    get: () => Promise<number>;
+                };
+                PositionState: {
+                    get: () => Promise<number>;
                 };
                 TargetPosition: {
-                    get: () => Promise<any>;
+                    get: () => Promise<number>;
                     set: (v: any) => Promise<void>;
                 };
             };
